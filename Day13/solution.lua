3630d37825c6bbcbe0c9ee934038ad29cfdb9b87
Unity2654 <k.jelic.74@gmail.com>
2024-12-13 18:53:01 +0000

    Solved Day13

--- Day13/solution.lua
+++ Day13/solution.lua
@@ -1,6 +1,6 @@
 -- Reading the input file
 machines = {}
-f = io.open("smallPuzzle.txt")
+f = io.open("puzzle.txt")
 line = true
 while line do
     machine = {}
@@ -16,10 +16,52 @@ while line do
     line = f:read("*line")
 end
 
-for _,m in ipairs(machines) do
-    for _,e in ipairs(m) do
-        io.write(e[1].." "..e[2].."\t")
+-- Calculating the solution of an equation
+function getFactors(machine)
+    X,Y,M = machine[1],machine[2],machine[3]
+    if Y[1]==0 or (X[2]-X[1]*(Y[2]/Y[1])) == 0 then
+        return nil,nil
     end
-    print()
+    e = (Y[2]/Y[1])
+    a = (M[2]-e*M[1])/(X[2]-X[1]*e)
+    b = (M[1]-a*X[1])/Y[1]
+    if #tostring(a) == #tostring(math.floor(a))+2 and #tostring(b) == #tostring(math.floor(b))+2 then
+        return a,b
+    end
+    return nil,nil
 end
 
+
+--
+-- Part 1
+--
+tokens = 0
+
+for i,m in ipairs(machines) do
+    a,b = getFactors(m)
+    if a and b then
+        tokens = tokens + 3*a + b
+    end
+end
+print(math.floor(tokens))
+
+--
+-- Part 2
+--
+
+function conversionErrorFix(machine)
+    machine[3][1] = machine[3][1]+ 10000000000000
+    machine[3][2] = machine[3][2]+ 10000000000000
+    return machine
+end
+
+moreTokens = 0
+
+for i,m in ipairs(machines) do
+    a,b = getFactors(conversionErrorFix(m))
+    if a and b then
+        moreTokens = moreTokens + 3*a + b
+    end
+end
+
+print(math.floor(moreTokens))
